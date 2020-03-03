ee69fb629a71042d27fce829b93648162734b145
xiaohuihuiCMJ <18882565275@163.com>
2020-03-03 15:49:24 +0800

    启动类修改

--- src/main/java/org/jeecg/JeecgApplication.java
+++ src/main/java/org/jeecg/JeecgApplication.java
@@ -1,12 +1,15 @@
 package org.jeecg;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @SpringBootApplication
+@ComponentScan(basePackages = {"org.jeecg","org.hero"})
+@MapperScan({"org.hero.*.mapper"})
 public class JeecgApplication {
 
     public static void main(String[] args) {
