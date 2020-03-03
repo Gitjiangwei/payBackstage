84b18d9c41e454476e3ee5c658a3847162232432
xiaohuihuiCMJ <18882565275@163.com>
2020-03-03 18:01:38 +0800

    创建

--- src/main/java/org/hero/renche/mapper/DictMapper.java
+++ src/main/java/org/hero/renche/mapper/DictMapper.java
@@ -1,11 +1,20 @@
 package org.hero.renche.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
 import org.hero.renche.entity.Dict;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * 字典 Mapper 接口
  *
  */
 public interface DictMapper extends BaseMapper<Dict> {
+
+    public List<Map<String,String>> queryDictItemsByCode(@Param("code") String code);
+
+    public String queryDictTextByKey(@Param("code") String code,@Param("key") String key);
+
 }
