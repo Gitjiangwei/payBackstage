be0938679ee56abbbdb6969508f39d7f05867def
Gitjiangwei <40350691+Gitjiangwei@users.noreply.github.com>
2020-03-08 14:27:34 +0800

    设备入库

--- src/main/java/org/hero/renche/service/imp/PurchaseServiceImpl.java
+++ src/main/java/org/hero/renche/service/imp/PurchaseServiceImpl.java
@@ -3,21 +3,30 @@ package org.hero.renche.service.imp;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import org.hero.renche.entity.EquipInfo;
 import org.hero.renche.entity.PurchaseInfo;
+import org.hero.renche.mapper.EquipInfoMapper;
 import org.hero.renche.mapper.PurchaseInfoMapper;
 import org.hero.renche.service.IPurchaseService;
+import org.hero.renche.thread.AsynTask;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 @Service
 public class PurchaseServiceImpl extends ServiceImpl<PurchaseInfoMapper,PurchaseInfo> implements IPurchaseService {
 
     @Autowired
     private PurchaseInfoMapper purchaseInfoMapper;
+
+    @Autowired
+    private EquipInfoMapper equipInfoMapper;
 
 
     @Transactional
@@ -51,4 +60,32 @@ public class PurchaseServiceImpl extends ServiceImpl<PurchaseInfoMapper,Purchase
         }
         return flag;
     }
+
+    @Override
+    public boolean insertReceiving(PurchaseInfo purchaseInfo) {
+        Boolean flag = false;
+        Map<String,String> receivingMap = new HashMap<String, String>();
+        receivingMap.put("equipCount",purchaseInfo.getQuantity());
+        receivingMap.put("equipPrice",purchaseInfo.getPrice());
+        receivingMap.put("equipModel",purchaseInfo.getItemModel());
+        receivingMap.put("equipName",purchaseInfo.getPurchaseItem());
+        receivingMap.put("purchaseId",purchaseInfo.getPurchaseId());
+        //查询当前库存的设备数量
+        int thisEquipCounts = equipInfoMapper.qryEquipKeyCount(purchaseInfo.getPurchaseId());
+        //List<EquipInfo> equipInfoList = equipInfoMapper.qryEquipListKey(equipInfo);
+        receivingMap.put("thisEquipCounts",String.valueOf(thisEquipCounts));
+        //执行线程
+        new AsynTask().asyncTask(receivingMap);
+        try {
+            Thread.sleep(1000);
+            flag = true;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return flag;
+    }
+
+
+
+
 }
