df7cae58e329b7e3dbeb3942a2b48b52f4bd549e
xiaohuihuiCMJ <18882565275@163.com>
2020-03-12 16:45:35 +0800

    修改工程点

--- src/main/java/org/hero/renche/service/imp/ProjectItemInfoServiceImpl.java
+++ src/main/java/org/hero/renche/service/imp/ProjectItemInfoServiceImpl.java
@@ -14,17 +14,23 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
+@Transactional
 public class ProjectItemInfoServiceImpl extends ServiceImpl<ProjectItemInfoMapper, ProjectItemInfo> implements IProjectItemInfoService {
 
     @Autowired
     private ProjectItemInfoMapper projectItemInfoMapper;
 
-    @Transactional
     @Override
     public PageInfo<ProjectItemVo> qryProjectItemInfo(ProjectItemInfo projectItemInfo, Integer page, Integer pageSize) {
 
         PageHelper.startPage(page,pageSize);
-        List<ProjectItemVo> projectItemInfoList = projectItemInfoMapper.qryListProjectItemInfo(projectItemInfo);
+        List<ProjectItemVo> projectItemInfoList = null;
+        try {
+            projectItemInfoList = projectItemInfoMapper.qryListProjectItemInfo(projectItemInfo);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+//        List<ProjectItemVo> projectItemInfoList = projectItemInfoMapper.qryListProjectItemInfo(projectItemInfo);
         return new PageInfo<ProjectItemVo>(projectItemInfoList);
     }
 
